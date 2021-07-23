71c1444c57ee51f1d942035da64ae6fcbcee0d4a
Chithra Priya <chithrapriyaj@gmail.com>
2021-07-23 16:36:53 -0700

    adding python api homework

---
+++ starter_code/api_keys.py
@@ -0,0 +1,7 @@
+# OpenWeatherMap API Key
+weather_api_key = "dfe3ab88e332997a742bccfbd5fa7f1c"
+
+# Google API Key
+g_key = "AIzaSyCO0XuqJt1Oi-8f2IiInw_nq7lLSa80u9U"
+
+api_key = "dfe3ab88e332997a742bccfbd5fa7f1c"
